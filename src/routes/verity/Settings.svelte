2a48d9bd3027a2e0c943f23294ed6e325d68d6ee
Oscar Lee-Vermeren <oscar.leevermeren@gmail.com>
2024-06-13 06:50:43 -0400

    refactor: improve settings menu

--- src/routes/verity/Settings.svelte
+++ src/routes/verity/Settings.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
 	import Settings from "lucide-svelte/icons/settings";
 	import { Button } from "$lib/components/ui/button";
+	import { Checkbox } from "$lib/components/ui/checkbox";
 	import * as DropdownMenu from "$lib/components/ui/dropdown-menu";
+	import { Label } from "$lib/components/ui/label";
 	import * as Tooltip from "$lib/components/ui/tooltip";
 	import { veritySettings } from "$lib/stores";
 </script>
@@ -21,14 +23,43 @@
 		</Tooltip.Root>
 	</DropdownMenu.Trigger>
 
-	<DropdownMenu.Content side="right">
-		<DropdownMenu.Label>Settings</DropdownMenu.Label>
-		<DropdownMenu.Separator />
-		<DropdownMenu.CheckboxItem bind:checked={$veritySettings.labels}>
-			Labels
-		</DropdownMenu.CheckboxItem>
-		<DropdownMenu.CheckboxItem bind:checked={$veritySettings.verify}>
-			Verify
-		</DropdownMenu.CheckboxItem>
+	<DropdownMenu.Content class="max-w-64" side="right">
+		<fieldset class="space-y-5 p-2">
+			<div class="flex items-start">
+				<div class="flex h-6 items-center">
+					<Checkbox
+						id="labels"
+						checked={$veritySettings.labels}
+						aria-describedby="labels-desc"
+						onCheckedChange={(value) => ($veritySettings.labels = Boolean(value))}
+					/>
+				</div>
+
+				<div class="ml-3">
+					<Label class="text-sm" for="labels">Labels</Label>
+					<p id="labels-desc" class="mt-0.5 text-xs">
+						Display shape names under their symbol.
+					</p>
+				</div>
+			</div>
+
+			<div class="flex items-start">
+				<div class="flex h-6 items-center">
+					<Checkbox
+						id="verify"
+						checked={$veritySettings.verify}
+						aria-describedby="verify-desc"
+						onCheckedChange={(value) => ($veritySettings.verify = Boolean(value))}
+					/>
+				</div>
+
+				<div class="ml-3">
+					<Label class="text-sm" for="verify">Verification</Label>
+					<p id="verify-desc" class="mt-0.5 text-xs">
+						Show outside shapes in between dissection steps to verify.
+					</p>
+				</div>
+			</div>
+		</fieldset>
 	</DropdownMenu.Content>
 </DropdownMenu.Root>
