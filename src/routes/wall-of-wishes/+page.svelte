9471ad7f9f932a6e9746b21da6303fde790409a2
Oscar Lee-Vermeren <oscar.leevermeren@gmail.com>
2024-06-15 01:26:41 -0400

    feat: add wall of wishes tool

---
+++ src/routes/wall-of-wishes/+page.svelte
@@ -0,0 +1,56 @@
+<script lang="ts">
+	import RotateCw from "lucide-svelte/icons/rotate-cw";
+	import { Button } from "$lib/components/ui/button";
+	import * as Select from "$lib/components/ui/select";
+	import Seo from "$lib/components/Seo.svelte";
+	import tools from "$lib/tools.json";
+	import wishes from "./wishes";
+
+	let selected: { value: number } | undefined;
+</script>
+
+<svelte:head>
+	<Seo
+		title="{tools['wall-of-wishes'].title} | The First Knife"
+		description={tools["wall-of-wishes"].description}
+		url="https://thefirstknife.vercel.app/wall-of-wishes"
+		image={tools["wall-of-wishes"].img}
+	/>
+</svelte:head>
+
+<div class="flex flex-col items-center justify-center">
+	<div class="mb-10 flex items-center gap-x-2">
+		<Select.Root bind:selected onSelectedChange={(item) => (selected = item)}>
+			<Select.Trigger class="w-[300px]">
+				<Select.Value placeholder="Select a wish" />
+			</Select.Trigger>
+
+			<Select.Content class="[font-feature-settings:'tnum']">
+				{#each wishes as wish, i}
+					<Select.Item value={i}>{i + 1}. {wish.description}</Select.Item>
+				{/each}
+			</Select.Content>
+		</Select.Root>
+
+		<Button size="icon" variant="outline" on:click={() => (selected = { value: -1 })}>
+			<RotateCw class="size-5" />
+		</Button>
+	</div>
+
+	<div class="grid grid-cols-5 grid-rows-4 gap-6">
+		{#each wishes[selected?.value ?? -1]?.pattern ?? { length: 5 } as row}
+			{#each row ?? { length: 4 } as symbol}
+				<div class="size-24 rounded-full bg-muted">
+					{#if symbol}
+						<img
+							src={symbol}
+							alt="Symbol for the wall of wishes"
+							width="96"
+							height="96"
+						/>
+					{/if}
+				</div>
+			{/each}
+		{/each}
+	</div>
+</div>
---
+++ src/routes/wall-of-wishes/symbols/index.ts
@@ -0,0 +1,38 @@
+import fishLeft from "./fish-left.png";
+import fishDown from "./fish-down.png";
+import fishCircle from "./fish-circle.png";
+import fishUp from "./fish-up.png";
+import snakeInfinity from "./snake-infinity.png";
+import snakeEight from "./snake-eight.png";
+import snakeSplit from "./snake-split.png";
+import snakeCloud from "./snake-cloud.png";
+import dragonFireLeft from "./dragon-fire-left.png";
+import dragonFireRight from "./dragon-fire-right.png";
+import dragonDead from "./dragon-dead.png";
+import dragonSpear from "./dragon-spear.png";
+import birdDiving from "./bird-diving.png";
+import birdBranch from "./bird-branch.png";
+import birdFlying from "./bird-flying.png";
+import birdStanding from "./bird-standing.png";
+
+export default [
+	fishLeft,
+	fishDown,
+	fishCircle,
+	fishUp,
+
+	snakeInfinity,
+	snakeEight,
+	snakeSplit,
+	snakeCloud,
+
+	dragonFireLeft,
+	dragonFireRight,
+	dragonDead,
+	dragonSpear,
+
+	birdDiving,
+	birdBranch,
+	birdFlying,
+	birdStanding,
+];
