76440506a42f1e035b5be273908c68659b57773b
Oscar Lee-Vermeren <oscar.leevermeren@gmail.com>
2024-06-17 18:10:01 -0400

    refactor: clean up header

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -10,22 +10,22 @@
 
 <header class="border border-b p-4 sm:px-6 md:px-10">
 	<div class="flex items-center justify-between">
-		<a class="flex items-center gap-x-3" href="/">
-			<img class="size-8 rounded" src="./logo.png" alt="Logo" width="128" height="128" />
+		<div class="flex items-center gap-x-3">
+			<a class="flex items-center gap-x-3" href="/">
+				<img class="size-8 rounded" src="/logo.png" alt="Logo" width="128" height="128" />
+				<h1 class="text-xl font-semibold">The First Knife</h1>
+			</a>
 
-			<h1 class="text-xl font-bold">The First Knife</h1>
 			<Separator class="h-6" orientation="vertical" />
 			<h2 class="text-xl font-light">{$subtitle}</h2>
-		</a>
+		</div>
 
-		<div class="flex items-center gap-x-6">
-			<div class="flex items-center gap-x-3">
-				<Label class="sr-only" for="theme-switch">Theme toggle</Label>
+		<div class="flex items-center gap-x-3">
+			<Label class="sr-only" for="theme-switch">Theme toggle</Label>
 
-				<Sun class="size-5 dark:text-foreground/20" />
-				<Switch id="theme-switch" checked={$mode === "dark"} onCheckedChange={toggleMode} />
-				<Moon class="size-5 text-foreground/20 dark:text-foreground" />
-			</div>
+			<Sun class="size-5 dark:text-foreground/20" />
+			<Switch id="theme-switch" checked={$mode === "dark"} onCheckedChange={toggleMode} />
+			<Moon class="size-5 text-foreground/20 dark:text-foreground" />
 		</div>
 	</div>
 </header>
