6a3bb47edb3c6f844262088fb70e5307807e244f
Oscar Lee-Vermeren <oscar.leevermeren@gmail.com>
2024-07-20 01:15:12 -0400

    touch up perk styles

--- src/lib/components/Perk.svelte
+++ src/lib/components/Perk.svelte
@@ -10,15 +10,15 @@
 	const { class: clasName, type, name, description, ...rest }: Props = $props();
 </script>
 
-<div class="relative w-[400px] backdrop-blur-[2px]">
+<div class="relative w-[400px] backdrop-blur-[1px]">
 	<div class="flex flex-col border-t-4 border-t-[#aeaeae] bg-black/85">
-		<div class="p-2.5">
-			<p class="text-xl/tight font-semibold uppercase tracking-wide">{name}</p>
+		<div class="p-2.5 pb-1.5">
+			<p class="text-xl/none font-medium uppercase tracking-wide">{name}</p>
 			<p class="text-sm font-light text-white/60">{type}</p>
 		</div>
 
-		<div class="bg-neutral-800/75 p-2.5">
-			<p class="text-sm font-light text-white/70">{description}</p>
+		<div class="bg-neutral-700/50 p-2.5">
+			<p class="text-sm/tight font-extralight text-white/80">{description}</p>
 		</div>
 	</div>
 </div>
